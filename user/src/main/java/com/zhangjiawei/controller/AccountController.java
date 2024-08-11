730eb80d48999f56aeebc9646d4db76690fdb61b
zhangjiawei <349071347@qq.com>
2024-08-11 18:44:33 +0800

    optimize code

--- user/src/main/java/com/zhangjiawei/controller/AccountController.java
+++ user/src/main/java/com/zhangjiawei/controller/AccountController.java
@@ -5,15 +5,11 @@ import com.zhangjiawei.dto.TestAllAccountReq;
 import com.zhangjiawei.service.AccountService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
 
 /**
  * @description:
